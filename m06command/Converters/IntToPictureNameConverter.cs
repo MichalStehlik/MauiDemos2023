ebc0e44ff32a76b41434d310f2f5796609c374e7
michal.stehlik <michal.stehlik@PC2022-01>
2023-03-20 09:35:46 +0100

    commands finished

---
+++ m06command/Converters/IntToPictureNameConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace m06command.Converters
+{
+    internal class IntToPictureNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int)
+            {
+                if((int)value < 4)
+                {
+                    return Pictures[(int)value];
+                    /*
+                    switch(value) 
+                    {
+                        case 0: return "vampire.png";
+                    }
+                    */
+                }
+            }
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string[] Pictures { get; set; }
+    }
+}
--- m06command/ViewModels/MainViewModel.cs
+++ m06command/ViewModels/MainViewModel.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace m06command.ViewModels
 {
@@ -12,14 +13,46 @@ namespace m06command.ViewModels
     {
         private int _position = 0;
 
+        public MainViewModel()
+        {
+            NextCommand = new Command(() =>
+            {
+                Position = Position + 1;
+            },
+            () => { return Position < 5; });
+            PreviousCommand = new Command(() =>
+            {
+                Position = Position - 1;
+            },
+            () => { return Position > 0; }
+            );
+            SetCommand = new Command<string>((value) =>
+            {
+                int v = Int32.Parse(value);
+                Position = v;
+            });
+        }
+
         public int Position
         {
             get { return _position; }
             set { 
                 _position = value; 
                 OnPropertyChanged();
+                (PreviousCommand as Command).ChangeCanExecute();
+                (NextCommand as Command).ChangeCanExecute();
             }
         }
+
+        public ICommand AlertCommand =>
+            new Command(() =>
+            {
+                App.Current.MainPage.DisplayAlert("Pozor", "Zpráva", "OK");
+            });
+
+        public ICommand NextCommand { get; private set; }
+        public ICommand PreviousCommand { get; private set; }
+        public ICommand SetCommand { get; private set; }
 
         #region MVVM
         public event PropertyChangedEventHandler PropertyChanged;
