99a77825a58e9fb73000c0ce1d1eb246b3f9972c
Michal Stehlík <michal.stehlik@pslib.cz>
2023-03-27 08:44:53 +0200

    styles

---
+++ m07styles/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace m07styles
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ m07styles/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace m07styles
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
