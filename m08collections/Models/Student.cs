d7b2770bbb822bc676edf2e19edb6eb70c13f36a
Michal Stehlík <michal.stehlik@pslib.cz>
2023-04-03 09:50:52 +0200

    collections2

--- m08collections/Models/Student.cs
+++ m08collections/Models/Student.cs
@@ -1,25 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace m08collections.Models
 {
-    internal class Student
+    internal class Student : INotifyPropertyChanged
     {
         private string _firstname;
         private string _lastname;
 
         public string FirstName {
             get { return _firstname; } 
-            set { _firstname = value; } 
+            set { _firstname = value; OnPropertyChanged(); } 
         }
         public string LastName
         {
             get { return _lastname; }
-            set { _lastname = value; }
+            set { _lastname = value; OnPropertyChanged(); }
         }
 
+        public override string ToString()
+        {
+            return FirstName + " " + LastName;
+        }
+
+        #region MVVM
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+        #endregion
     }
 }
--- m08collections/ViewModels/MainViewModel.cs
+++ m08collections/ViewModels/MainViewModel.cs
@@ -1,18 +1,57 @@
 ﻿using m08collections.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace m08collections.ViewModels
 {
     internal class MainViewModel : INotifyPropertyChanged
     {
-        private List<Student> _students;
+        private Student _selected;
+        private ObservableCollection<Student> _students;
+        public ICommand CreateCommand { get; private set; }
+        public ICommand RemoveCommand { get; private set; }
+        public MainViewModel()
+        {
+            Students = new ObservableCollection<Student>
+            {
+                new Student {FirstName = "Alois", LastName = "Aladin"},
+                new Student {FirstName = "Beáta", LastName = "Blatná"}
+            };
+            CreateCommand = new Command(() =>
+            {
+                Students.Add(new Student { FirstName = "Ctirad", LastName = "Cudný" });
+            });
+            RemoveCommand = new Command<Student>((s) =>
+            {
+                Students.Remove(s);
+                SelectedStudent = null;
+            },
+            (s) =>
+            {
+                return s is not null;
+            });
+        }
 
+        public ObservableCollection<Student> Students { 
+            get { return _students; }
+            set { _students = value; OnPropertyChanged(); }
+        }
+        public Student SelectedStudent
+        {
+            get { return _selected; }
+            set { 
+                _selected = value; 
+                OnPropertyChanged(); 
+                (RemoveCommand as Command).ChangeCanExecute(); 
+            }
+        }
 
         #region MVVM
         public event PropertyChangedEventHandler PropertyChanged;
