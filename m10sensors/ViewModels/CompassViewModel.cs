55d0c6b0ab1f26568e9137d5a904d9b6b2e380d2
Michal Stehlík <michal.stehlik@pslib.cz>
2023-04-24 09:47:59 +0200

    location

--- m10sensors/ViewModels/CompassViewModel.cs
+++ m10sensors/ViewModels/CompassViewModel.cs
@@ -10,6 +10,53 @@ namespace m10sensors.ViewModels
 {
     internal class CompassViewModel: INotifyPropertyChanged
     {
+        private string _value;
+        public CompassViewModel()
+        {
+            OnPropertyChanged(nameof(Supported));
+            SwitchCommand = new Command(() =>
+            {
+                if (!Monitoring)
+                {
+                    // Turn on compass
+                    Compass.Default.ReadingChanged += Compass_ReadingChanged;
+                    Compass.Default.Start(SensorSpeed.UI);
+                }
+                else
+                {
+                    // Turn off compass
+                    Compass.Default.Stop();
+                    Compass.Default.ReadingChanged -= Compass_ReadingChanged;
+                }
+                OnPropertyChanged(nameof(Monitoring));
+                OnPropertyChanged(nameof(Status));
+            },
+            () => { return Supported; });
+        }
+
+        public bool Supported { get { return Compass.IsSupported; } }
+        public bool Monitoring { get { return Compass.IsMonitoring; } }
+        public string Value { 
+            get { 
+                return _value; 
+            } 
+            set { 
+                _value = value; 
+                OnPropertyChanged(); 
+            } 
+        }
+        public string Status { get 
+            {
+                if (!Supported) return "Senzor není k dispozici";
+                if (Monitoring) return "Vypnout";
+                return "Zapnout";
+            } }
+        public Command SwitchCommand { get; private set; }
+        private void Compass_ReadingChanged(object sender, CompassChangedEventArgs e)
+        {
+            // Update UI Label with compass state
+            Value = $"Compass: {e.Reading}";
+        }
         #region MVVM
         public event PropertyChangedEventHandler PropertyChanged;
         protected void OnPropertyChanged([CallerMemberName] string propName = null)
--- m10sensors/ViewModels/LocationViewModel.cs
+++ m10sensors/ViewModels/LocationViewModel.cs
@@ -10,6 +10,142 @@ namespace m10sensors.ViewModels
 {
     internal class LocationViewModel : INotifyPropertyChanged
     {
+        private double? _latitude;
+        private double? _longitude;
+        private double? _altitude;
+        private string _status;
+        private bool _isWorking;
+
+        public LocationViewModel()
+        {
+            Status = "Nic";
+            ReadCachedCommand = new Command(
+                async () =>
+                {
+                    try
+                    {
+                        Status = "Pracuji";
+                        Location location = await Geolocation.Default.GetLastKnownLocationAsync();
+
+                        if (location != null)
+                        {
+                            Latitude = location.Latitude;
+                            Longitude = location.Longitude;
+                            Altitude = location.Altitude;
+                        }
+                        Status = "Hotovo";
+                    }
+                    catch (FeatureNotSupportedException fnsEx)
+                    {
+                        Status = "Geolokace není podporována.";
+                    }
+                    catch (FeatureNotEnabledException fneEx)
+                    {
+                        Status = "Geolokace není zapnuta.";
+                    }
+                    catch (PermissionException pEx)
+                    {
+                        Status = "Nedostateční práva.";
+                    }
+                    catch (Exception ex)
+                    {
+                        Status = "Chyba: " + ex.Message;
+                    }
+                }
+                );
+            ReadCommand = new Command(
+                async () =>
+                {
+                    try
+                    {
+                        Status = "Pracuji";
+                        CancellationTokenSource _cancel;
+                        GeolocationRequest request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+
+                        _cancel = new CancellationTokenSource();
+
+                        Location location = await Geolocation.Default.GetLocationAsync(request, _cancel.Token);
+
+                        if (location != null)
+                        {
+                            Latitude = location.Latitude;
+                            Longitude = location.Longitude;
+                            Altitude = location.Altitude;
+                        }
+                        Status = "Hotovo";
+                    }
+                    catch (FeatureNotSupportedException fnsEx)
+                    {
+                        Status = "Geolokace není podporována.";
+                    }
+                    catch (FeatureNotEnabledException fneEx)
+                    {
+                        Status = "Geolokace není zapnuta.";
+                    }
+                    catch (PermissionException pEx)
+                    {
+                        Status = "Nedostateční práva.";
+                    }
+                    catch (Exception ex)
+                    {
+                        Status = "Chyba: " + ex.Message;
+                    }
+                }
+                );
+
+        }
+
+        public double? Latitude { 
+            get { 
+                return _latitude; 
+            }
+            set {
+                _latitude = value;
+                OnPropertyChanged();
+            } 
+        }
+
+        public double? Longitude
+        {
+            get
+            {
+                return _longitude;
+            }
+            set
+            {
+                _longitude = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double? Altitude
+        {
+            get
+            {
+                return _altitude;
+            }
+            set
+            {
+                _altitude = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Status
+        {
+            get
+            {
+                return _status;
+            }
+            set
+            {
+                _status = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Command ReadCachedCommand { get; private set; }
+        public Command ReadCommand { get; private set; }
         #region MVVM
         public event PropertyChangedEventHandler PropertyChanged;
         protected void OnPropertyChanged([CallerMemberName] string propName = null)
